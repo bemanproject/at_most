c85738f694c7a3ba72c686fbf59ab59a5f224030
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-04-02 02:32:02 +0300

    ci: add sample test to fix ci

---
+++ tests/beman/at_most/at_most.test.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/at_most/at_most.hpp>
+#include <gtest/gtest.h>
+
+TEST(AtMostTest, BlankTest) {
+    EXPECT_TRUE(true);
+}
